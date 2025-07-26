013f4f5c4adee802cda83fd1eecf24d25074313f
minoritas13 <nafisrizqullah12@gmail.com>
2025-07-27 01:25:32 +0700

    commit SIPEGAS 1.0.6

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import laravel from 'laravel-vite-plugin';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
-    base: '/build/', 
+    base: '/build/',
     plugins: [
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js'],
@@ -11,4 +11,8 @@ export default defineConfig({
         }),
         tailwindcss(),
     ],
+    server: {
+        https: true,
+        host: '0.0.0.0',
+    },
 });
